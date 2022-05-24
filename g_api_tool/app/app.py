b13a8607871e23d167437c32f82cd4e8979c0c1c
evil-des <latsplaym@gmail.com>
2022-05-24 13:41:56 +0300

    fixed config

--- g_api_tool/app/app.py
+++ g_api_tool/app/app.py
@@ -1,5 +1,33 @@
 import asyncio
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from g_api_tool.misc import config
 
 
 async def run():
-    pass
+    try:
+        service = build('sheets', 'v4', credentials=config["app"]["google_api_token"])
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
+    except HttpError as err:
+        print(err)
+
+
+run()
---
+++ g_api_tool/misc.py
@@ -0,0 +1,4 @@
+import json
+
+with open("config/config.json") as json_data:
+    config = json.load(json_data)
